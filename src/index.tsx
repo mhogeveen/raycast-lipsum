15875519bffdb308eeedc67f22471a1572e9d27b
Maarten Hogeveen <maarten@Maartens-MBP.home>
2021-12-05 14:59:53 +0100

    Add search bar logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import { List, ActionPanel, CopyToClipboardAction, PasteAction, Icon } from '@raycast/api';
 
+import { useState } from 'react';
+
 const Command = (): JSX.Element => {
+  const [count, setCount] = useState<number>(1);
+
+  const handleSearchTextChange = (text: string) => {
+    const textToInt = parseInt(text, 10);
+    const zeroIfNaN = isNaN(textToInt) ? 1 : textToInt;
+    setCount(zeroIfNaN);
+  }
+
   return (
     <List
       searchBarPlaceholder={'Enter an amount of lipsum to generate'}
+      onSearchTextChange={handleSearchTextChange}
     >
     </List>
   );
